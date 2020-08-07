99002f919fbaf3a47d665c4e685d8bdf17472070
LeeSheng <727151354@qq.com>
2020-08-07 08:42:50 +0800

    first commit

---
+++ AutomaticBrake/AutomaticBrakeMain.cpp
@@ -0,0 +1,9 @@
+//
+//  AutomaticBrakeMain.cpp
+//  AutomaticBrake
+//
+//  Created by Lee Sheng on 2020/8/6.
+//  Copyright © 2020 Lee Sheng. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ AutomaticBrake/catch.h
@@ -0,0 +1,13 @@
+//
+//  catch.h
+//  AutomaticBrake
+//
+//  Created by Lee Sheng on 2020/8/6.
+//  Copyright © 2020 Lee Sheng. All rights reserved.
+//
+
+#ifndef catch_h
+#define catch_h
+
+
+#endif /* catch_h */
